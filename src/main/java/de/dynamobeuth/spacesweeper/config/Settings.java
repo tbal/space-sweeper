00199938089d0a1a540fc567ad9adf9a9376b9fc
Karsten Kosmala <kosmala@cosmocode.de>
2017-07-07 19:00:04 +0200

    enable default skin and add firebase database url

--- src/main/java/de/dynamobeuth/spacesweeper/config/Settings.java
+++ src/main/java/de/dynamobeuth/spacesweeper/config/Settings.java
@@ -1,12 +1,12 @@
 package de.dynamobeuth.spacesweeper.config;
 
 public class Settings {
-    public static final String SKIN = "bunt";
+    public static final String SKIN = "default";
     public static final int COL_COUNT = 3;
     public static final int COL_WIDTH = 100;
     public static final int COL_HEIGHT = 600;
     public static final int RADIUS = COL_WIDTH / 2;
     public static final int PLAYER_MOVE_SPEED = 300; // in millis
     public static final int SPRITE_SPEED = 2500; // in millis
-
+    public static final String DATABASE_URL = "https://space-sweeper.firebaseio.com";
 }
